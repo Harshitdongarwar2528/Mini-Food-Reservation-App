5a05e6a829f8afc9e1c8ec001eb570aee9e8ae76
Harshit Dongarwar <harshitdongarwar2003@gmail.com>
2025-11-19 21:40:58 +0530

    Initial Commit

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "RESTRAUNT",
+    })
+    .then(() => {
+      console.log("Connected to database SuccessFully!");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecing to database: ${err}`);
+    });
+};
+
---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              At our core, the only thing we’re truly serious about is food. Every dish we create is crafted with passion, fresh ingredients, and an unwavering commitment to flavor. We believe great food has the power to bring people together, spark joy, and create unforgettable moments. Whether you’re here for a quick bite or a memorable dining experience, our mission is simple — serve food that makes you smile and keeps you coming back for more.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
